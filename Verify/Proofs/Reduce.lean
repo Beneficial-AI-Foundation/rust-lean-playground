71dab0713cd79faf275d57bf10d7ca13c33a14b0
Oliver Butterley <oliver.butterley@gmail.com>
2025-09-21 23:14:48 +0200

    tiny things

--- Verify/Proofs/Reduce.lean
+++ Verify/Proofs/Reduce.lean
@@ -10,7 +10,7 @@ open Std.Tactic
 set_option mvcgen.warning false
 
 /-- Automatically identify `(5:usize).toNat` and `5` in array sizes -/
-instance {α : Type} : Coe (RustArray α (5:usize)) (RustArray α 5) where
+instance {α : Type} : Coe (RustArray α (5 : usize)) (RustArray α 5) where
   coe x := x.cast (by simp)
 
 -- Theorem to upstream?
@@ -29,8 +29,8 @@ def p : Nat := 2^255 - 19
 
 /-- Postcondition -/
 def post (limbs result : RustArray u64 5) :=
-  (∀ i, (h:i < 5) → result[i] ≤ (2^51 + (2^13 - 1) * 19).toUInt64)
-  ∧ ArrayU645_to_Nat limbs ≡ ArrayU645_to_Nat result [MOD p]
+  (∀ i, (h : i < 5) → result[i] ≤ (2^51 + (2^13 - 1) * 19).toUInt64) ∧
+  ArrayU645_to_Nat limbs ≡ ArrayU645_to_Nat result [MOD p]
 
 set_option maxHeartbeats 5000000 in
 -- Simp is expensive here
@@ -45,7 +45,7 @@ theorem reduce.spec (limbs : (RustArray u64 (5 : usize))) :
   -- Discard array access side conditions
   all_goals simp [Vector.size, -Int.reducePow, -Nat.reducePow] at *
   -- Discard arithmetic overflows
-  all_goals try (subst_vars; simp at * ; bv_decide)
+  all_goals try (subst_vars; simp at *; bv_decide)
   -- Remains to show the post condition
   constructor
   · -- All the limbs of the result are bounded
@@ -53,13 +53,12 @@ theorem reduce.spec (limbs : (RustArray u64 (5 : usize))) :
     all_goals subst_vars; simp; bv_decide
   · -- The result is equal [Mod p] the input
     subst_vars; simp [-Int.reducePow, -Nat.reducePow, Finset.range] at *
-    rw [show 2251799813685247 = 2 ^ 51 - 1 by simp]
+    rw [show 2251799813685247 = 2^51 - 1 by simp]
     -- Masking is remainder
-    have h_mask : ∀ (x : u64) (y:Nat), x.toNat &&& (2^y - 1) = x.toNat % 2^y := by simp
-    repeat rw [h_mask]
+    have (x : u64) (y : Nat) : x.toNat &&& (2^y - 1) = x.toNat % 2^y := by simp
+    repeat rw [this]
     -- Remove `% 2^64` on results of arithmetic operations
-    have := UInt64.shiftRight_lt 51 (by simp)
-    repeat rw [Nat.mod_eq_of_lt (b := 2^64) (by grind)]
+    repeat rw [Nat.mod_eq_of_lt (b := 2^64) (by have := UInt64.shiftRight_lt 51; grind)]
     -- Unfold and finish
     unfold p Nat.ModEq
     omega
