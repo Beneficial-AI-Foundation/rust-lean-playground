f9193af80c374ffbcaa88672be8188a38f2be9ba
Oliver Butterley <oliver.butterley@gmail.com>
2025-09-30 14:56:25 +0200

    dvd 8 good

--- verify/Verify/Proofs/clamp_integer.lean
+++ verify/Verify/Proofs/clamp_integer.lean
@@ -3,7 +3,7 @@ import Verify.Src.RustLeanPlayground
 import Mathlib
 import Verify.Proofs.Aux
 
-set_option linter.style.longLine false
+set_option linter.style.setOption false
 set_option maxHeartbeats 1000000
 
 /-! # clamp_integer
@@ -15,22 +15,48 @@ open rust_lean_playground
 
 attribute [-simp] Int.reducePow Nat.reducePow
 
+/- Using the specs with bit-vectors -/
+attribute [-progress] U8.add_spec U8.mul_spec
+attribute [local progress] U8.add_bv_spec U8.mul_bv_spec
+
+
+/-! ## Auxillary theorems -/
+
+theorem dvd_and_248 (byte : U8) : 8 ∣ (byte &&& 248#u8).val := by
+  -- 248 = 0b11111000, so and with 248 clears the lowest 3 bits
+  sorry
+
 /-! ## Auxillary defs required for specs -/
 
 /-- Auxiliary definition to interpret a vector of u32 as an integer -/
 @[simp]
-def ArrayU645_to_Nat (limbs : Array U64 5#usize) : Nat :=
-  ∑ i ∈ Finset.range 5, 2^(51 * i) * (limbs[i]!).val
+def ArrayU832.as_Nat (bytes : Array U8 32#usize) : Nat :=
+  ∑ i ∈ Finset.range 32, 2^(8 * i) * (bytes[i]!).val
 
-/-- Curve25519 is the elliptic curve over the prime field with order p -/
-def p : Nat := 2^255 - 19
+/-- The cofactor of curve25519 -/
+@[simp]
+def h : Nat := 8
 
 /-! ## Spec for `clamp_integer` -/
 
 /-- **Spec and proof concerning `clamp_integer`**:
 - No panic
-- To do: The result is equal to the input mod p. -/
+- to do: 2^254 ≤ as_nat_32_u8 result
+- to do: as_nat_32_u8 result < 2^255
+- (as_nat_32_u8 result) is divisible by 8 (the cofactor of curve25519)
+-/
 theorem clamp_integer_spec (bytes : Array U8 32#usize) :
-    ∃ result, clamp_integer bytes = ok (result) := by
+    ∃ result, clamp_integer bytes = ok (result) ∧
+    h ∣ (ArrayU832.as_Nat result)  := by
   unfold clamp_integer
   progress*
+  simp
+  apply Finset.dvd_sum
+  intro i hi
+  by_cases hc : i = 0
+  · -- case i = 0
+    subst_vars
+    simpa [i1_post_1] using dvd_and_248 _
+  · -- Case when 0 < i
+    have := List.mem_range.mp hi
+    interval_cases i <;> omega
