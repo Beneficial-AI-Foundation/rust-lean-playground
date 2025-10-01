26e69e37aa71fec4208172dd2bd43c46bcd58179
Oliver Butterley <oliver.butterley@gmail.com>
2025-10-01 18:43:38 +0200

    m

---
+++ verify/Verify/Proofs/M.lean
@@ -0,0 +1,23 @@
+import Verify.Src.RustLeanPlayground
+
+/-! # M
+
+The main statement concerning `m` is `m_spec` (below).
+-/
+
+open Aeneas.Std Result
+open rust_lean_playground
+
+attribute [-simp] Int.reducePow Nat.reducePow
+
+/-! ## Spec for `m` -/
+
+/-- **Spec for `m`**:
+- Does not overflow and hence returns a result
+- The result equals the product of the two input values -/
+theorem m_spec (x y : U64) :
+    ∃ result, m x y = ok (result) ∧
+    result.val = x.val * y.val := by
+  unfold m
+  progress*
+  simp [*]; scalar_tac
